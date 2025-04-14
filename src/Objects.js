b1a244f5ecf45b4ac88aa8b453422eb414f84475
veveronika <vera.suleymanova.05@bk.ru>
2025-04-14 22:34:02 +0300

    Задание по HTML

--- src/Objects.js
+++ src/Objects.js
@@ -1,26 +1,34 @@
-/*
-  В функцию personUpdate() приходят данные в виде объекта, содержащую некую информацию о человеке.
-  Если этот человек является женщиной (свойство gender содержит значение 'female'), то из этого объекта
-  необходимо удалить свойство age, если оно есть.
-  Если этот человек является мужчиной (свойство gender содержит значение 'male'), следует убедиться,
-  что в этом объекте есть свойство income. Если его нет, необходимо его добавить
-  и присвоить начальное значение 100000.
-  Объект после манипуляций следует вернуть в качестве результата работы функции.
-*/
 export function personUpdate(data) {
+  if (data.gender === 'female') {
+    if ('age' in data) {
+      delete data.age;
+    }
+  } else if (data.gender === 'male') {
+    if (!('income' in data)) {
+      data.income = 100000;
+    }
+  }
+  return data;
 }
 
-/*
-  В функцию objectFieldsList приходят три объекта с различными полями, список которых заранее неизвестен.
-  Верните список названий этих полей в алфавитном порядке в виде массива строк.
-*/
 export function objectFieldsList(obj1, obj2, obj3) {
+  const fields = new Set();
+
+  [obj1, obj2, obj3].forEach(obj => {
+    Object.keys(obj).forEach(key => fields.add(key));
+  });
+
+  return Array.from(fields).sort();
 }
 
-/*
-  Верните в результате работы функции массив с клонами объекта obj.
-  При этом каждый клон должен дополнительно содержать поле id со своим порядковым номером в массиве.
-  Количество клонов - count.
-*/
 export function objectClone(obj, count) {
+  const clones = [];
+
+  for (let i = 0; i < count; i++) {
+    const clone = JSON.parse(JSON.stringify(obj));
+    clone.id = i + 1;
+    clones.push(clone);
+  }
+
+  return clones;
 }
